117bd9736836e0a4e58154f8bb34b952eb698b0b
yayapple <empmn1@gmail.com>
2023-11-03 17:07:28 -0500

    test cors

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 // warning: this is practically my foray into js 
 // i do not know formatting conventions
 
+// misc
+function getCurrentCentralTime() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://www.timeapi.io/api/TimeZone/zone?timeZone=America/Chicago");
+    xhr.send();
+    xhr.onload = function() {
+        if (xhr.status != 200) {
+            console.log(xhr.status);
+        } else {
+            console.log(xhr.response);
+        }
+    };
+    
+    xhr.onerror = function(e) {
+        console.log("request failed: " + e);
+    };
+}
+
+
+getCurrentCentralTime();
+
+
+
+
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -21,9 +46,14 @@ window.addEventListener("mousemove", e => {
     cursor.y = e.pageY;
 })
 
+window.addEventListener("touchmove", e => {
+    cursor.x = e.targetTouches[0].pageX;
+    cursor.y = e.targetTouches[0].pageY;
+})
+
 // vector offsets
 
-function calc_offset(x1, y1, x2, y2) {
+function calcOffset(x1, y1, x2, y2) {
 
     if (isNaN(cursor.x) || isNaN(cursor.y)) return [x1, y1];
 
@@ -38,7 +68,7 @@ function calc_offset(x1, y1, x2, y2) {
     return [x1 + dx, y1 + dy];
 }
 /*
-function offset_card() {
+function offsetCard() {
     var card = document.getElementById("center-card");
     const direction = Math.atan2(canvas.height / 2 - cursor.y, canvas.width / 2 - cursor.x);
     const magnitude = Math.hypot(canvas.height / 2 - cursor.y, canvas.width / 2 - cursor.x);
@@ -51,7 +81,7 @@ function offset_card() {
     card.style.marginLeft = offset_x + "px";
 }
 */
-function resize_text() {
+function resizeText() {
 
     const card_width = document.getElementById("center-card").offsetWidth;
 
@@ -95,12 +125,12 @@ const color = "rgb(117, 33, 20)"
 
     // (re)generate array of dots
 var dots = new Array;
-function init_canvas() {
+function initCanvas() {
     console.log(radius)
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     setTimeout(() => {
-        resize_text();
+        resizeText();
     }, 200);
 
     radius = (canvas.width > 2500) ? 4 : 2; // adapt params to screen size, should probably format this better
@@ -125,14 +155,14 @@ function init_canvas() {
 }
 
     // draw circle given coordinates and radius
-function draw_circle(x, y, r) {
+function drawCircle(x, y, r) {
     ctx.moveTo(x, y);
     ctx.arc(x, y, r, 0, Math.PI * 2, true);
     
 }
 
     // regenerate dot array when resize window
-window.addEventListener("resize", init_canvas);
+window.addEventListener("resize", initCanvas);
 
     // update canvas
 const fps = 40;
@@ -142,15 +172,15 @@ function update() {
     }, 1000 / fps);
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // offset_card();
+    // offsetCard();
 
     ctx.beginPath();
     for (const coords of dots) {
-        const [x_offset, y_offset] = calc_offset(coords[0], coords[1], cursor.x, cursor.y);
-        draw_circle(x_offset, y_offset, radius);
+        const [x_offset, y_offset] = calcOffset(coords[0], coords[1], cursor.x, cursor.y);
+        drawCircle(x_offset, y_offset, radius);
     }
     ctx.fill();
 }
 
-init_canvas();
+initCanvas();
 update();
